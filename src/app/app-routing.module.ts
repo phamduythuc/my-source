5dd1f91c170219e4bf595d9025a9b157306b61a3
thuc duy <thucphamduy182@gmail.com>
2023-04-03 10:19:50 +0700

    signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { TestComponentComponent } from './layouts/test-component/test-component.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './modules/auth/sign-in/sign-in.component';
+import {LayoutComponent} from "./layout/layout.component";
+import {AuthGuard} from "./core/auth/guand/auth.guard";
 
 const routes: Routes = [
     // {
@@ -18,12 +20,12 @@ const routes: Routes = [
     {
         path: 'signin',
         loadChildren: () => import('./modules/auth/sign-in/sign-in.module').then(m => m.SignInModule)
-    }
+    },
 
   { path: '', component: SignInComponent },
-  { path: 'second', component: TestComponentComponent },
 
-  { path: 'name', loadChildren: () => import('./modules/auth/sign-up/sign-up.module').then(m => m.SignUpModule) },
+
+  { path: 'sign-up', loadChildren: () => import('./modules/auth/sign-up/sign-up.module').then(m => m.SignUpModule) },
 
   { path: 'forgot-password', loadChildren: () => import('./modules/auth/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
 ];
